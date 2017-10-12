f871f520745406b0a3e506eadadaa7a22d0d6453
toreluntang <toreluntang@gmail.com>
2017-10-12 12:21:49 +0200

    Added postgresql sink to write to the cloudbased sql db at elephantsql

--- facebook-flink-analysis/src/main/java/dk/itu/thesis/KafkaFacebookAnalysis.java
+++ facebook-flink-analysis/src/main/java/dk/itu/thesis/KafkaFacebookAnalysis.java
@@ -13,8 +13,11 @@ import edu.stanford.nlp.trees.Tree;
 import edu.stanford.nlp.util.CoreMap;
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.common.functions.FoldFunction;
+import org.apache.flink.api.common.functions.MapFunction;
 import org.apache.flink.api.java.functions.KeySelector;
+import org.apache.flink.api.java.io.jdbc.JDBCOutputFormat;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.datastream.KeyedStream;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
@@ -22,8 +25,10 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer010;
 import org.apache.flink.streaming.util.serialization.SimpleStringSchema;
+import org.apache.flink.types.Row;
 import org.apache.flink.util.Collector;
 
+import java.sql.Types;
 import java.util.Properties;
 
 //import org.apache.flink.streaming.connectors.json.JSONParser;
@@ -33,7 +38,32 @@ public class KafkaFacebookAnalysis {
     public static void main(String[] args) throws Exception {
 
 
+        // the host and the port to connect to
+        String kafkahostname = null;
+        String groupid = null;
+        String topic = null;
+        String user = null;
+        String password = null;
 
+        try {
+            final ParameterTool params = ParameterTool.fromArgs(args);
+            kafkahostname = params.getRequired("kafkahost");
+            groupid = params.getRequired("groupid");
+            topic = params.getRequired("topic");
+            user = params.getRequired("sqluser");
+            password = params.getRequired("sqlpass");
+
+        } catch (Exception e) {
+            if (null == kafkahostname)
+                System.err.println("No kafka host specified. Please add --kafkahost <kafka hostname>");
+
+            if (null == groupid)
+                System.err.println("No kafka group.id specified. Please add --groupid <kafka hostname>");
+
+            if (null == topic)
+                System.err.println("No kafka topic specified. Please add --topic <topic>");
+            return;
+        }
 
 
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
@@ -41,14 +71,14 @@ public class KafkaFacebookAnalysis {
 
         // Kafka setup
         Properties properties = new Properties();
-        properties.setProperty("bootstrap.servers", "10.26.51.31:9092");
-        properties.setProperty("group.id", "wiki-result");
+        properties.setProperty("bootstrap.servers", kafkahostname);
+        properties.setProperty("group.id", groupid);
 
 
         // Read raw strings (list of json objects) "[ {nyhed, likes}, {nyhed, likes}, {...} ]"
         DataStream<String> messageStream = env.addSource(
                 new FlinkKafkaConsumer010<>(
-                        "wiki-result",
+                        topic,
                         new SimpleStringSchema(),
                         properties));
 
@@ -83,7 +113,7 @@ public class KafkaFacebookAnalysis {
         );
 
 
-        DataStream<Tuple2<String, String>> result = keyedStream
+        DataStream<Tuple2<String, String>> messageSentimentTupleStream = keyedStream
                 .timeWindow(Time.seconds(5))
                 .fold(new Tuple2<>("", ""), new FoldFunction<JsonObject, Tuple2<String, String>>() {
                     @Override
@@ -94,15 +124,41 @@ public class KafkaFacebookAnalysis {
                     }
                 });
 
+//        messageSentimentTupleStream.print();
+//
+        String query = "INSERT INTO result (message, sentiment) VALUES (?, ?);";
 
-        result.print();
-//        messageStream.print();
+        JDBCOutputFormat jdbcOutput = JDBCOutputFormat.buildJDBCOutputFormat()
+                .setDrivername("org.postgresql.Driver")
+                .setBatchInterval(1)
+                .setDBUrl("jdbc:postgresql://elmer.db.elephantsql.com:5432/fapuqfvg")
+                .setUsername(user)
+                .setPassword(password)
+                .setQuery(query)
+                .setSqlTypes(new int[] { Types.VARCHAR, Types.VARCHAR }) //set the types
+                .finish();
+
+
+        SingleOutputStreamOperator<Row> resultRow = messageSentimentTupleStream
+                .map(new MapFunction<Tuple2<String, String>, Row>() {
+                    @Override
+                    public Row map(Tuple2<String, String> msgSentTuple) throws Exception {
+                        Row row = new Row(2);
+                        row.setField(0, msgSentTuple.f0.substring(0, 512));
+                        row.setField(1, msgSentTuple.f1);
+                        return row;
+                    }
+                });
+
+
+        resultRow.writeUsingOutputFormat(jdbcOutput);
 
 
         env.execute();
 
-        System.out.println("Au revoir");
+//        System.out.println("Au revoir");
     }
+
 
     public static String getSentiment(String text) {
 
