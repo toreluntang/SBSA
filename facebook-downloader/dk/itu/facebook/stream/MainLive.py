90143540cf3a7972add904c24ec090a32c6ba773
toreluntang <toreluntang@gmail.com>
2017-10-26 13:22:26 +0200

    tokenized array based on the news text added to the message for easy access when predicting in flink with keras model

--- facebook-downloader/dk/itu/facebook/stream/MainLive.py
+++ facebook-downloader/dk/itu/facebook/stream/MainLive.py
@@ -3,6 +3,7 @@ import time
 import argparse
 import json
 from confluent_kafka import Producer
+from keras.preprocessing.text import Tokenizer
 
 
 def check_int(val):
@@ -12,14 +13,39 @@ def check_int(val):
         raise argparse.ArgumentParser("Value {} from input has to be an integer.".format(val))
     return ival
 
+
+def preprocessing_tokenize(X, num_words=1000):
+    _tokenizer = Tokenizer(num_words=num_words,
+                           filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n',
+                           lower=True,
+                           split=" ",
+                           char_level=False)
+
+    _tokenizer.fit_on_texts(X)
+    _X = _tokenizer.texts_to_matrix(X)
+
+    return _X
+
+
+def get_tokenized_data(json_message):
+    try:
+        news_text = json_message['message'] + json_message['name']
+    except:
+        news_text = ""
+    return preprocessing_tokenize([news_text])
+
+
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
-    parser.add_argument('--token', dest='fb_app_token', help='The facebook API key, which can be fetched from https://developers.facebook.com/tools/explorer/145634995501895/')
-    parser.add_argument('--page_id', dest='fb_page_id', help='The page id from facebook. From https://www.facebook.com/FoxNews/ the page id is FoxNews.')
+    parser.add_argument('--token', dest='fb_app_token',
+                        help='The facebook API key, which can be fetched from https://developers.facebook.com/tools/explorer/145634995501895/')
+    parser.add_argument('--page_id', dest='fb_page_id',
+                        help='The page id from facebook. From https://www.facebook.com/FoxNews/ the page id is FoxNews.')
     parser.add_argument('--server', dest='kafka_bootstrap_server', help='The kafka server')
     parser.add_argument('--topic', dest='kafka_topic', help='The topic the message should be produced as')
-    parser.add_argument('--sleep', dest='sleep_time', default=300, help='Sleep between each request to facebook for the newest post', type=check_int)
+    parser.add_argument('--sleep', dest='sleep_time', default=300,
+                        help='Sleep between each request to facebook for the newest post', type=check_int)
 
     args = parser.parse_args()
 
@@ -37,12 +63,16 @@ if __name__ == "__main__":
 
     while 1:
         print("Fetching newest post")
-        post_and_reactions = graph.get_object(fb_page_id, fields='username,posts.limit(1){message,description,caption,name,reactions.type(LIKE).limit(0).summary(true).as(LIKE),reactions.type(LOVE).limit(0).summary(true).as(LOVE),reactions.type(WOW).limit(0).summary(true).as(WOW),reactions.type(HAHA).limit(0).summary(true).as(HAHA),reactions.type(SAD).limit(0).summary(true).as(SAD),reactions.type(ANGRY).limit(0).summary(true).as(ANGRY), created_time}')
+        post_and_reactions = graph.get_object(fb_page_id,
+                                              fields='username,posts.limit(1){message,description,caption,name,reactions.type(LIKE).limit(0).summary(true).as(LIKE),reactions.type(LOVE).limit(0).summary(true).as(LOVE),reactions.type(WOW).limit(0).summary(true).as(WOW),reactions.type(HAHA).limit(0).summary(true).as(HAHA),reactions.type(SAD).limit(0).summary(true).as(SAD),reactions.type(ANGRY).limit(0).summary(true).as(ANGRY), created_time}')
 
         current_id = post_and_reactions['posts']['data'][0]['id']
         print("Newest post has id {} and previous post has id {}".format(current_id, previous_id))
 
         if current_id != previous_id:
+            preprocessed_data = get_tokenized_data(post_and_reactions['posts']['data'][0])
+            post_and_reactions['posts']['data'][0]['tokenized_data'] = preprocessed_data.tolist()
+
             previous_id = current_id
             p.produce(kafka_topic, json.dumps(post_and_reactions['posts']['data'][0]))
             print("Message has been produced")
