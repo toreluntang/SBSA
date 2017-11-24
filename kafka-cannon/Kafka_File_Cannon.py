53e4a7ad7bf683ccda832429f0c3d508856e9673
Alexander <alewge@gmail.com>
2017-11-24 19:10:35 +0100

    add cannone

---
+++ kafka-cannon/Kafka_File_Cannon.py
@@ -0,0 +1,4 @@
+
+str = '{"message": "WORKING TOGETHER, we will defeat this opioid epidemic and free our nation from the terrible affliction of drug abuse. http://45.wh.gov/OpioidCrisis", "LIKE": {"data": [], "summary": {"total_count": 9876, "viewer_reaction": "NONE"}}, "LOVE": {"data": [], "summary": {"total_count": 1169, "viewer_reaction": "NONE"}}, "WOW": {"data": [], "summary": {"total_count": 29, "viewer_reaction": "NONE"}}, "HAHA": {"data": [], "summary": {"total_count": 136, "viewer_reaction": "NONE"}}, "SAD": {"data": [], "summary": {"total_count": 41, "viewer_reaction": "NONE"}}, "ANGRY": {"data": [], "summary": {"total_count": 22, "viewer_reaction": "NONE"}}, "created_time": "2017-10-27T12:34:00+0000", "id": "1wierd.test"}'
+for i in range(0,10000000):
+    print(str)
